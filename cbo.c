391918ecae11baa5e7792a32d49088e2bb93b51d
BayashiPascal <bayashipascal@gmail.com>
2021-06-12 12:06:14 +0900

    Modif CharBeforeDot to allow space before dot

--- cbo.c
+++ cbo.c
@@ -121,7 +121,7 @@ const char* cboErrorTypeStr[] = {
   "Several consecutive blank lines",
   "Unbalanced curly brace at head of line",
   "Unbalanced curly brace at tail of line",
-  "'.' must be after [a-zA-Z0-9])], or be at head of line",
+  "'.' must be after [a-zA-Z0-9 ])], or be at head of line",
   "No space before opening curly brace",
   "No empty line before comment",
   "Indent level is incorrect",
@@ -3235,7 +3235,7 @@ bool CBoFileCheckNoCurlyBraceAtTail(
 
 }
 
-// Check that '.' are surrounded by [a-zA-Z0-9])] or are at head of line
+// Check that '.' are surrounded by [a-zA-Z0-9 ])] or are at head of line
 // on the lines of the CBoFile 'that' with the CBo 'cbo'
 // Return true if there was no problem, else false
 bool CBoFileCheckCharBeforeDot(
@@ -3346,6 +3346,7 @@ bool CBoFileCheckCharBeforeDot(
               !(line->str[iChar - 1] >= 'a' && line->str[iChar - 1] <= 'z') &&
               !(line->str[iChar - 1] >= 'A' && line->str[iChar - 1] <= 'Z') &&
               !(line->str[iChar - 1] >= '0' && line->str[iChar - 1] <= '9') &&
+              line->str[iChar - 1] != ' ' &&
               line->str[iChar - 1] != ']' &&
               line->str[iChar - 1] != ')'))) {
 
